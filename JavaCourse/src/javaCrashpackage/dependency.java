3d931d48f1071679de97d838d9eac98865cdc18b
UshapriyaJ <101462743+UshapriyaJ@users.noreply.github.com>
2022-03-12 12:26:53 -0500

    testsuite added

---
+++ JavaCourse/src/javaCrashpackage/dependency.java
@@ -0,0 +1,20 @@
+package SeleniumtestNG;
+
+import org.testng.annotations.Test;
+
+public class dependency {
+	//dependencies
+	    @Test(enabled = true)
+	    public void tenth() {
+	    	System.out.println("tenth");
+	    }
+	    @Test(dependsOnMethods = "tenth")
+	    public void highschool() {
+	    	System.out.println("12th");
+	    }
+	    @Test(dependsOnMethods = "highschool")
+	    public void college() {
+	    	System.out.println("college");
+	    }
+	    
+}
